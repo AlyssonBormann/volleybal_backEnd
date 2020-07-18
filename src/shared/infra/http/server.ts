f3c6e4c2ac9f94a1b4adbf9f1f64c7b7310290cc
Alysson Bormannn <alysson.bormann@gmail.com>
2020-07-17 23:25:32 -0400

    Criado as pastas com conexao ao banco para ja comecar a desenvolver

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response, NextFunction, response } from 'express';
+
+import AppError from '@shared/errors/AppError';
+import routes from './routes';
+import '@shared/infra/typeorm';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use((err:Error, request: Request, resposne: Response, _:NextFunction)=>{
+  if( err instanceof AppError){
+    return response.status(err.statusCode).json({
+      status:'error',
+      message: err.message
+    })
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Erro do servidor interno - Fez bosta 💩 rsrsrsr',
+  });
+});
+
+
+
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+});
